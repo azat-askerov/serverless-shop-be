e9b7cbab764992687eb1de5dd99834c8deef58d6
Azat Askerov <azat.askerov@tvh.com>
2022-07-17 21:17:52 +0600

    task 3: Task 3 is done

---
+++ product-service/src/functions/getProduct/handler.ts
@@ -0,0 +1,18 @@
+import { formatErrorResponse, ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+import products from '../../mocks/products';
+
+const getProductHandler: ValidatedEventAPIGatewayProxyEvent<{}> = async (event) => {
+  const { productId } = event.pathParameters;
+
+  if (products[productId]) {
+    return formatJSONResponse({
+      product: products[productId],
+    });
+  } else {
+    return formatErrorResponse('No such a product found!', 404);
+  }
+};
+
+export const main = middyfy(getProductHandler);
---
+++ product-service/src/functions/getProductsList/handler.ts
@@ -0,0 +1,12 @@
+import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+import { formatJSONResponse } from '@libs/api-gateway';
+import { middyfy } from '@libs/lambda';
+import products from '../../mocks/products';
+
+const getProductsList: ValidatedEventAPIGatewayProxyEvent<{}> = async (event) => {
+  return formatJSONResponse({
+    products,
+  });
+};
+
+export const main = middyfy(getProductsList);
